1a8619c7ae79244df47397f849ee490b0f3dd11b
Massimo Guida <massimo@massimoguida.com>
2020-06-13 21:25:06 -0400

    Updates

---
+++ precache-manifest.42bc8e9b82aff4b54ad675f3ae6e542e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ecb0f8d8367b6ffd6d974e32645e780",
+    "url": "/assignment19/employeedirectory/public/index.html"
+  },
+  {
+    "revision": "89739bc03e5a39001cf0",
+    "url": "/assignment19/employeedirectory/public/static/css/main.029f4300.chunk.css"
+  },
+  {
+    "revision": "85bcfed149514bbc3cd4",
+    "url": "/assignment19/employeedirectory/public/static/js/2.97c5b96a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/assignment19/employeedirectory/public/static/js/2.97c5b96a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89739bc03e5a39001cf0",
+    "url": "/assignment19/employeedirectory/public/static/js/main.c509bdca.chunk.js"
+  },
+  {
+    "revision": "deb4f00d2249fd0331bf",
+    "url": "/assignment19/employeedirectory/public/static/js/runtime-main.c2473444.js"
+  },
+  {
+    "revision": "06d638c9ad6d4cdcf39ce7fc7c9fedfc",
+    "url": "/assignment19/employeedirectory/public/static/media/business-company-working.06d638c9.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/assignment19/employeedirectory/public/precache-manifest.42bc8e9b82aff4b54ad675f3ae6e542e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/assignment19/employeedirectory/public/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
